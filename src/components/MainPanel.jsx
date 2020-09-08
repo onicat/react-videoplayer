99d894a953f802ab59aad1a9419c4b37127d1114
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-08 15:39:23 +0300

    Moved SearchInputForm to MainPanel

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import SearchInputForm from './SearchInputForm';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Paper, InputBase, IconButton, } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
+import AppsIcon from '@material-ui/icons/Apps';
 
 const useStyles = makeStyles({
   toolbar: {
     display: 'flex',
     justifyContent: 'center'
+  },
+  searchForm: {
+    width: '400px',
+    padding: '2px 5px',
+    display: 'flex'
+  },
+  searchInput: {
+    padding: '0 5px',
+    flexGrow: 1
   }
 });
 
@@ -17,7 +27,15 @@ const MainPanel = () => {
   return (
     <AppBar position='fixed'>
       <Toolbar className={classes.toolbar}>
-        <SearchInputForm/>
+        <Paper className={classes.searchForm}>
+          <IconButton>
+            <AppsIcon/>
+          </IconButton>
+          <InputBase placeholder='Search' className={classes.searchInput}/>
+          <IconButton>
+            <SearchIcon/>
+          </IconButton>
+        </Paper>
       </Toolbar>
     </AppBar>
   );
