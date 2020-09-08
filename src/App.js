92df757456f39dd40797b3ea72a34004750d78c3
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-08 15:21:37 +0300

    Connected created components to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from 'react'
+import { ThemeProvider, styled } from '@material-ui/core/styles';
+
+import MainPanel from './components/MainPanel'
+import Player from 'components/Player'
+import theme from 'theme';
+import { Box } from '@material-ui/core';
+
+const PlayerContainer = styled(Box)({
+  height: 'calc(100vh - 64px)',
+  paddingTop: '64px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
+
 const App = () => {
-  return null;
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <MainPanel></MainPanel>
+        <PlayerContainer>
+          <Player/>
+        </PlayerContainer>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
