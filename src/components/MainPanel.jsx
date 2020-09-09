4e8ed2715c2b0f793fe97d46e4de4bbba305533b
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-09 04:14:00 +0300

    Changed MainPanel shape

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,60 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles, Paper, InputBase, IconButton, Popover, } from '@material-ui/core';
+import { makeStyles, InputBase, IconButton, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search'
-import AppsIcon from '@material-ui/icons/Apps';
 
-import AppsMenu from './AppsMenu';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: 'none'
+  },
   toolbar: {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   searchForm: {
-    width: '400px',
-    padding: '2px 5px',
-    display: 'flex'
+    paddingBottom: '5px',
+    borderBottom: `1px solid ${theme.palette.text.primary}`
   },
   searchInput: {
-    padding: '0 5px',
-    flexGrow: 1
+    width: '400px',
+    padding: '0 5px'
   }
-});
+}));
 
 const MainPanel = () => {
-  const [anchorAppsPopover, setAnchorAppsPopover] = useState(null);
   const classes = useStyles();
   
-  const handleAppsButtonClick = event => {
-    setAnchorAppsPopover(event.currentTarget);
-  };
-
-  const handleAppsPopoverClosing = () => {
-    setAnchorAppsPopover(null);
-  };
-  
   return (
-    <AppBar position='fixed'>
+    <AppBar className={classes.root} color='transparent' position='fixed'>
       <Toolbar className={classes.toolbar}>
-        <Paper className={classes.searchForm}>
-          <IconButton onClick={handleAppsButtonClick}>
-            <AppsIcon/>
-          </IconButton>
-          <InputBase placeholder='Search' className={classes.searchInput}/>
-          <IconButton>
-            <SearchIcon/>
-          </IconButton>
-        </Paper>
-        <Popover 
-            open={Boolean(anchorAppsPopover)}
-            anchorEl={anchorAppsPopover}
-            onClose={handleAppsPopoverClosing}
-            anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
-          >
-            <AppsMenu/>
-        </Popover>
+        <Box className={classes.searchForm}>
+          <InputBase
+            placeholder='Search' 
+            className={classes.searchInput}
+            endAdornment={
+              <IconButton size='small'>
+                <SearchIcon/>
+              </IconButton>
+            }
+          />
+        </Box>
       </Toolbar>
     </AppBar>
   );
