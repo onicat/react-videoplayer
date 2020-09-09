8f1451ac55b347d912f9e13a8672e123c2388280
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-09 15:42:12 +0300

    Created file manager Drawer

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles, InputBase, IconButton, Box } from '@material-ui/core';
+import { makeStyles, InputBase, IconButton, Box, Drawer } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search'
 import StorageIcon from '@material-ui/icons/Storage';
 
@@ -30,7 +30,8 @@ const useStyles = makeStyles((theme) => ({
 
 const MainPanel = () => {
   const classes = useStyles();
-  
+  const [isFileManagerVisible, toggleFileManagerDrawer] = useState(false);
+
   return (
     <AppBar className={classes.root} color='transparent' position='fixed'>
       <Toolbar className={classes.toolbar}>
@@ -45,9 +46,19 @@ const MainPanel = () => {
             }
           />
         </Box>
-        <IconButton className={classes.storageButton}>
+        <IconButton 
+          onClick={toggleFileManagerDrawer.bind(null, true)}
+          className={classes.storageButton}
+        >
           <StorageIcon/>
         </IconButton>
+        <Drawer 
+          anchor='right'
+          open={isFileManagerVisible}
+          onClose={toggleFileManagerDrawer.bind(null, false)}
+        >
+          
+        </Drawer>
       </Toolbar>
     </AppBar>
   );
