ef742dce4ae7e824e85778a8719765d47224463d
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-13 18:31:31 +0300

    Changed getFolderPathNode logic

--- src/logic/getFolderPathNode.js
+++ src/logic/getFolderPathNode.js
@@ -2,10 +2,10 @@ const getFolderPathNode = (virtualFolderPath, paths) => {
   if (virtualFolderPath.length === 0) return paths;
   
   const pathStack = virtualFolderPath.split('/');
-  let currentNode = paths[pathStack[0]];
+  let currentNode = paths[pathStack[0]].paths;
 
   for (let depth = 1; depth < pathStack.length; depth++) {
-    currentNode = currentNode.paths[pathStack[depth]];
+    currentNode = currentNode[pathStack[depth]].paths;
   }
 
   return currentNode;
